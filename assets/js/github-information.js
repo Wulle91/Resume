50c99417c0cc33c3b5880f2d082dcee52836eeb1
Wulle91 <mvuckovi91@gmail.com>
2023-04-05 16:01:27 +0000

    Fix bugs for the gh-user-data & gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -31,12 +31,14 @@ function repoInformationHTML(repos) {
                     <strong>Repo List:</strong>
                 </p>
                 <ul>
-                    ${listItemsHTML.join('\n')}
+                    ${listItemsHTML.join("\n")}
                 </ul>
             </div>`
 }
 
 function fetchGitHubInformation(event) {
+    $('#gh-user-data').html("");
+    $('#gh-repo-data').html("");
 
     var username = $('#gh-username').val();
     if(!username) {
@@ -70,3 +72,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
